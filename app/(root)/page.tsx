411ca427a631e627fbf7d40ad1aaab4e2d39ab0e
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2025-03-27 19:47:02 +0300

    header added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="font-main">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+    </div>
+  );
+}
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME=process.env.APP_NAME || "Shoes Store"
+export const APP_DESC=process.env.APP_DESC || 'Modern website in Next js'
+export const SERVER_URL=process.env.APP_SERVER_URL || "http://localhost:3000/"
